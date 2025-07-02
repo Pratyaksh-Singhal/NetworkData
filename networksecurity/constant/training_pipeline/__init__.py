391175cf044a37b02f60abe83e35a642226b155b
Pratyaksh-Singhal <pratyaskhsinghal635@gmail.com>
2025-07-02 16:00:46 +0530

    Data Transformation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -45,6 +45,7 @@ PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 """
 Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
 """
+
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"

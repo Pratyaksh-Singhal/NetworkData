0f0f2d58f3ac0bafb05b8b45b94287e1b96961d2
Pratyaksh-Singhal <pratyaskhsinghal635@gmail.com>
2025-07-13 21:03:45 +0530

    model training

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -64,7 +64,6 @@ DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 """
 Model Trainer ralated constant start with MODE TRAINER VAR NAME
 """
-
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
